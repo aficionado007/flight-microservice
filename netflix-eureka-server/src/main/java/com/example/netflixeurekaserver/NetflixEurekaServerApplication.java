4aa5ce211138137ad36efbae1b4b0851e2509023
Bakshi <Geetika.Bakshi@team.telstra.com>
2019-09-26 12:49:16 +0530

    Eureks

---
+++ netflix-eureka-server/src/main/java/com/example/netflixeurekaserver/NetflixEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.example.netflixeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NetflixEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixEurekaServerApplication.class, args);
+	}
+
+}
